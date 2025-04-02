203219e436e9023916a06e8b05506cc4799e1585
Amanda  Jenkins <amandajenkins@Amandas-MacBook-Pro-14.local>
2025-04-02 14:33:57 -0400

    change .h file

--- lab3-sw/vga_ball.h
+++ lab3-sw/vga_ball.h
@@ -3,19 +3,29 @@
 
 #include <linux/ioctl.h>
 
+/* Struct for RGB color */
 typedef struct {
-  unsigned char red, green, blue;
+    unsigned char red, green, blue;
 } vga_ball_color_t;
-  
 
+/* Struct for H/V position */
 typedef struct {
-  vga_ball_color_t background;
+    unsigned short h;
+    unsigned short v;
+} vga_ball_hv_t;
+
+/* Combined argument structure for ioctl */
+typedef struct {
+    vga_ball_color_t background;
+    vga_ball_hv_t hv;
 } vga_ball_arg_t;
 
 #define VGA_BALL_MAGIC 'q'
 
-/* ioctls and their arguments */
+/* IOCTLs */
 #define VGA_BALL_WRITE_BACKGROUND _IOW(VGA_BALL_MAGIC, 1, vga_ball_arg_t)
 #define VGA_BALL_READ_BACKGROUND  _IOR(VGA_BALL_MAGIC, 2, vga_ball_arg_t)
+#define VGA_BALL_WRITE_HV         _IOW(VGA_BALL_MAGIC, 3, vga_ball_arg_t)
+#define VGA_BALL_READ_HV          _IOR(VGA_BALL_MAGIC, 4, vga_ball_arg_t)
 
-#endif
+#endif /* _VGA_BALL_H */
